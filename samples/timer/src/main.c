c44eae28f20f85161faae2b1c33458ab333d2cb6
Andreas Moltumyr <andreas.moltumyr@nordicsemi.no>
2024-11-27 10:30:13 +0100

    samples: add timer sample
    Add timer sample. It shows how to use the lite_timer library.
    
    Signed-off-by: Andreas Moltumyr <andreas.moltumyr@nordicsemi.no>

---
+++ samples/timer/src/main.c
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2025 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <lite_timer.h>
+#include <zephyr/sys/printk.h>
+
+#define PERIODIC_TIMER_TICKS LITE_TIMER_MS_TO_TICKS(CONFIG_PERIODIC_TIMER_INTERVAL_MS)
+#define HELLO_TIMER_TICKS    LITE_TIMER_MS_TO_TICKS(CONFIG_HELLO_TIMER_DURATION_MS)
+#define WORLD_TIMER_TICKS    LITE_TIMER_MS_TO_TICKS(CONFIG_WORLD_TIMER_DURATION_MS)
+#define BYE_TIMER_TICKS      LITE_TIMER_MS_TO_TICKS(CONFIG_BYE_TIMER_DURATION_MS)
+
+static struct lite_timer oneshot_timer;
+static struct lite_timer periodic_timer;
+static bool done;
+
+static const char *const hello_str = "Hello";
+static const char *const world_str = "world!";
+static const char *const bye_str = "bye!\n";
+
+/**
+ * @brief Timeout handler for single-shot timer.
+ *
+ * Restarts the oneshot timer two times with different strings. Then stops the periodic timer.
+ */
+static void oneshot_timeout_handler(void *context)
+{
+	static int cnt;
+	int err;
+	const char *const str = (const char *const)context;
+
+	printk("%s", str);
+
+	if (cnt == 0) {
+		err = lite_timer_start(&oneshot_timer, WORLD_TIMER_TICKS, (void *)world_str);
+		if (err) {
+			printk("Failed to start oneshot timer, err %d\n", err);
+		}
+	} else if (cnt == 1) {
+		err = lite_timer_start(&oneshot_timer, BYE_TIMER_TICKS, (void *)bye_str);
+		if (err) {
+			printk("Failed to start oneshot timer, err %d\n", err);
+		}
+	} else {
+		err = lite_timer_stop(&periodic_timer);
+		if (err) {
+			printk("Failed to stop periodic timer, err %d\n", err);
+		}
+
+		done = true;
+	}
+
+	cnt++;
+}
+
+/**
+ * @brief Timeout handler for repeated timer.
+ *
+ * Prints punctuation on timeout.
+ */
+static void periodic_timeout_handler(void *context)
+{
+	ARG_UNUSED(context);
+
+	printk(".");
+}
+
+int main(void)
+{
+	int err;
+
+	err = lite_timer_init(&periodic_timer, LITE_TIMER_MODE_REPEATED, periodic_timeout_handler);
+	if (err) {
+		printk("Failed to initialize periodic timer, err %d\n", err);
+		return -1;
+	}
+
+	err = lite_timer_init(&oneshot_timer, LITE_TIMER_MODE_SINGLE_SHOT, oneshot_timeout_handler);
+	if (err) {
+		printk("Failed to initialize oneshot timer, err %d\n", err);
+		return -1;
+	}
+
+	err = lite_timer_start(&periodic_timer, PERIODIC_TIMER_TICKS, NULL);
+	if (err) {
+		printk("Failed to start periodic timer, err %d\n", err);
+		return -1;
+	}
+
+	err = lite_timer_start(&oneshot_timer, HELLO_TIMER_TICKS, (void *)hello_str);
+	if (err) {
+		printk("Failed to start oneshot timer, err %d\n", err);
+		return -1;
+	}
+
+	printk("Timer sample started\n");
+
+	while (!done) {
+		/* Sleep */
+		__WFE();
+	}
+
+	return 0;
+}
