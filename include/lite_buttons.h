32fd6dfab7716c19a00a2678021f23a00a4944f9
Mirko Covizzi <mirko.covizzi@nordicsemi.no>
2024-12-18 13:12:49 +0100

    lib: lite_buttons: add buttons library
    Adds buttons library.
    
    Signed-off-by: Mirko Covizzi <mirko.covizzi@nordicsemi.no>

---
+++ include/lite_buttons.h
@@ -0,0 +1,146 @@
+/*
+ * Copyright (c) 2012-2025 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+/** @file
+ *
+ * @defgroup lite_buttons NCS-Lite Buttons library
+ * @{
+ *
+ * @brief Buttons management module.
+ *
+ * @details The lite_buttons library uses the nrfx_gpiote to detect that a button has been
+ *          pressed. To handle debouncing, it will start a lite_timer.
+ *          The button will only be reported as pressed if the corresponding pin is still active
+ *          when the timer expires.
+ *          If there is a new GPIOTE event while the timer is running, the timer is restarted.
+ */
+
+#ifndef LITE_BUTTONS_H__
+#define LITE_BUTTONS_H__
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <nrfx_gpiote.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum lite_buttons_event_type {
+	/* Indicates that a button is released. */
+	LITE_BUTTONS_RELEASE = 0,
+	/* Indicates that a button is pressed. */
+	LITE_BUTTONS_PRESS = 1
+};
+
+enum lite_buttons_active_state {
+	/* Indicates that a button is active low. */
+	LITE_BUTTONS_ACTIVE_LOW = 0,
+	/* Indicates that a button is active high. */
+	LITE_BUTTONS_ACTIVE_HIGH = 1
+};
+
+/**
+ * @brief Enumerator used for selecting the pin to be pulled down or up at the time of pin
+ *        configuration.
+ */
+enum lite_buttons_pin_pull {
+	/* Pin pull-up resistor disabled. */
+	LITE_BUTTONS_PIN_NOPULL = NRF_GPIO_PIN_NOPULL,
+	/* Pin pull-down resistor enabled. */
+	LITE_BUTTONS_PIN_PULLDOWN = NRF_GPIO_PIN_PULLDOWN,
+	/* Pin pull-up resistor enabled. */
+	LITE_BUTTONS_PIN_PULLUP = NRF_GPIO_PIN_PULLUP,
+};
+
+/**
+ * @brief Button event handler type.
+ */
+typedef void (*lite_buttons_handler_t)(uint8_t pin_number, enum lite_buttons_event_type action);
+
+/**
+ * @brief Button configuration.
+ */
+struct lite_buttons_config {
+	/* Pin to be used as a button. */
+	uint8_t pin_number;
+	/* LITE_BUTTONS_ACTIVE_HIGH or LITE_BUTTONS_ACTIVE_LOW. */
+	enum lite_buttons_active_state active_state;
+	/* Pull-up or pull-down configuration. */
+	enum lite_buttons_pin_pull pull_config;
+	/* Handler to be called when the button is pressed. */
+	lite_buttons_handler_t handler;
+};
+
+/**
+ * @brief Initialize buttons.
+ *
+ * @details This function will initialize the specified pins as buttons, and configure them.
+ *
+ * @note The @p configs configurations are referenced internally so it is the user's responsibility
+ *       to take care of their lifetime.
+ *
+ * @note The @ref lite_buttons_enable function must be called in order to enable the button
+ *       detection.
+ *
+ * @param[in] configs Array of button configurations to be used.
+ * @param[in] num_configs Number of button configurations.
+ * @param[in] detection_delay Delay (in microseconds) from a GPIOTE event until a button is reported
+ *                            as pressed. Must be higher than twice @ref LITE_TIMER_MIN_TIMEOUT_US.
+ *
+ * @retval 0 on success.
+ * @retval -NRF_EPERM If the lite_buttons library is already initialized.
+ * @retval -NRF_EINVAL If input data is invalid.
+ * @retval -NRF_EIO If an error occurred.
+ */
+int lite_buttons_init(const struct lite_buttons_config *configs, uint8_t num_configs,
+		      uint32_t detection_delay);
+
+/**
+ * @brief Deinitialize buttons.
+ *
+ * @details This function will deinitialize the buttons library.
+ *
+ * @retval 0 on success.
+ * @retval -NRF_EPERM If the lite_buttons library is already initialized.
+ * @retval -NRF_EIO If an error occurred.
+ */
+int lite_buttons_deinit(void);
+
+/**
+ * @brief Enable button detection.
+ *
+ * @retval 0 on success.
+ * @retval -NRF_EPERM If the lite_buttons library is not initialized.
+ */
+int lite_buttons_enable(void);
+
+/**
+ * @brief Disable button detection.
+ *
+ * @retval 0 on success.
+ * @retval -NRF_EPERM If the lite_buttons library is not initialized.
+ * @retval -NRF_EIO If an error occurred.
+ */
+int lite_buttons_disable(void);
+
+/**
+ * @brief Check if a button is being pressed.
+ *
+ * @param[in] pin Pin to be checked.
+ *
+ * @return true if the specified button is pressed, false otherwise.
+ */
+bool lite_buttons_is_pressed(uint8_t pin);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* LITE_BUTTONS_H__ */
+
+/** @} */
---
+++ lib/lite_buttons/lite_buttons.c
@@ -0,0 +1,354 @@
+/*
+ * Copyright (c) 2012-2025 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <zephyr/logging/log.h>
+#include <zephyr/irq.h>
+#include <nrfx_gpiote.h>
+
+#include <lite_timer.h>
+#include <lite_buttons.h>
+
+LOG_MODULE_REGISTER(lite_buttons, CONFIG_LITE_BUTTONS_LOG_LEVEL);
+
+#define GPIOTE_INST  0
+#define IRQ_PRIO     3
+#define BITS_PER_PIN 4
+#define NUM_PINS     CONFIG_LITE_BUTTONS_NUM_PINS
+
+/*
+ * In this module, a state machine is used for each pin (button).
+ * Since the GPIOTE PORT event is shared among all pins, individual pin events might be missed.
+ * To ensure reliable detection, the module relies on the GPIOTE interrupt only to activate a
+ * periodic `lite_timer` that samples the state of each pin.
+ * When all buttons are in the idle state (i.e., there are no active buttons), the timer is stopped
+ * to save power.
+ *
+ * State transitions are based on the sampled values of the buttons.
+ * The state machine has the following transitions:
+ *
+ * -----------------------------------------------------
+ * | value | current state    | new state              |
+ * |---------------------------------------------------|
+ * |  0    | IDLE             | IDLE                   |
+ * |  1    | IDLE             | PRESS_ARMED            |
+ * |  0    | PRESS_ARMED      | IDLE                   |
+ * |  1    | PRESS_ARMED      | PRESS_DETECTED         |
+ * |  1    | PRESS_DETECTED   | PRESSED (push event)   |
+ * |  0    | PRESS_DETECTED   | PRESS_ARMED            |
+ * |  0    | PRESSED          | RELEASE_DETECTED       |
+ * |  1    | PRESSED          | PRESSED                |
+ * |  0    | RELEASE_DETECTED | IDLE (release event)   |
+ * |  1    | RELEASE_DETECTED | PRESSED                |
+ * -----------------------------------------------------
+ *
+ */
+enum button_state {
+	BUTTON_IDLE,
+	BUTTON_PRESS_ARMED,
+	BUTTON_PRESS_DETECTED,
+	BUTTON_PRESSED,
+	BUTTON_RELEASE_DETECTED
+};
+
+static const nrfx_gpiote_t gpio_instance = NRFX_GPIOTE_INSTANCE(GPIOTE_INST);
+
+struct lite_buttons_state {
+	bool is_init;
+	struct lite_buttons_config const *configs;
+	uint8_t num_configs;
+	uint32_t detection_delay;
+	struct lite_timer timer;
+	uint8_t pin_states[NUM_PINS * BITS_PER_PIN / 8];
+	uint64_t pin_active;
+};
+
+static struct lite_buttons_state global;
+
+static enum button_state state_get(uint8_t pin)
+{
+	uint8_t pair_state = global.pin_states[pin >> 1];
+	uint8_t state = (pin & 0x1) ? (pair_state >> BITS_PER_PIN) : (pair_state & 0x0F);
+
+	return (enum button_state)state;
+}
+
+static struct lite_buttons_config const *button_get(uint8_t pin)
+{
+	for (int i = 0; i < global.num_configs; i++) {
+		struct lite_buttons_config const *config = &global.configs[i];
+
+		if (pin == config->pin_number) {
+			return config;
+		}
+	}
+
+	return NULL;
+}
+
+static void state_set(uint8_t pin, uint8_t state)
+{
+	uint8_t mask = (pin & 1) ? 0x0F : 0xF0;
+	uint8_t state_mask = (pin & 1) ? ((uint8_t)state << BITS_PER_PIN) : (uint8_t)state;
+
+	global.pin_states[pin >> 1] &= mask;
+	global.pin_states[pin >> 1] |= state_mask;
+}
+
+static void user_event(uint8_t pin, enum lite_buttons_event_type type)
+{
+	struct lite_buttons_config const *config = button_get(pin);
+
+	if (config && config->handler) {
+		LOG_DBG("Pin %d %s", pin, (type == LITE_BUTTONS_PRESS) ? "pressed" : "released");
+		config->handler(pin, type);
+	}
+}
+
+static void evt_handle(uint8_t pin, uint8_t value)
+{
+	switch (state_get(pin)) {
+	case BUTTON_IDLE:
+		if (value) {
+			LOG_DBG("Pin %d idle->armed", pin);
+			state_set(pin, BUTTON_PRESS_ARMED);
+			global.pin_active |= 1ULL << pin;
+		} else {
+			/* stay in IDLE */
+		}
+		break;
+	case BUTTON_PRESS_ARMED:
+		state_set(pin, value ? BUTTON_PRESS_DETECTED : BUTTON_IDLE);
+		LOG_DBG("Pin %d armed->%s", pin, value ? "detected" : "idle");
+		break;
+	case BUTTON_PRESS_DETECTED:
+		if (value) {
+			state_set(pin, BUTTON_PRESSED);
+			user_event(pin, LITE_BUTTONS_PRESS);
+		} else {
+			state_set(pin, BUTTON_PRESS_ARMED);
+		}
+		LOG_DBG("Pin %d detected->%s", pin, value ? "pressed" : "armed");
+		break;
+	case BUTTON_PRESSED:
+		if (value == 0) {
+			LOG_DBG("Pin %d pressed->release_detected", pin);
+			state_set(pin, BUTTON_RELEASE_DETECTED);
+		} else {
+			/* stay in pressed */
+		}
+		break;
+	case BUTTON_RELEASE_DETECTED:
+		if (value) {
+			state_set(pin, BUTTON_PRESSED);
+		} else {
+			state_set(pin, BUTTON_IDLE);
+			user_event(pin, LITE_BUTTONS_RELEASE);
+			global.pin_active &= ~(1ULL << pin);
+		}
+		LOG_DBG("Pin %d release_detected->%s", pin, value ? "pressed" : "idle");
+		break;
+	}
+}
+
+static void timer_start(void)
+{
+	int err;
+
+	err = lite_timer_start(&global.timer, global.detection_delay / 2, NULL);
+	if (err) {
+		LOG_WRN("Failed to start app_timer (err:%d)", err);
+	}
+}
+
+static int buttons_disable(void)
+{
+	int err;
+
+	err = lite_timer_stop(&global.timer);
+	if (err) {
+		return -EIO;
+	}
+
+	for (int i = 0; i < global.num_configs; i++) {
+		nrfx_gpiote_trigger_enable(&gpio_instance, global.configs[i].pin_number, false);
+	}
+
+	global.pin_active = 0;
+
+	return 0;
+}
+
+static void detection_delay_timeout_handler(void *ctx)
+{
+	bool is_set;
+	bool is_active;
+	struct lite_buttons_config const *config;
+
+	for (int i = 0; i < global.num_configs; i++) {
+		config = &global.configs[i];
+		is_set = nrfx_gpiote_in_is_set(config->pin_number);
+		is_active = !((config->active_state == LITE_BUTTONS_ACTIVE_HIGH) ^ is_set);
+
+		evt_handle(config->pin_number, is_active);
+	}
+
+	if (global.pin_active) {
+		timer_start();
+	} else {
+		LOG_DBG("No active buttons, stopping timer");
+	}
+}
+
+static void gpiote_event_handler(nrfx_gpiote_pin_t pin, nrfx_gpiote_trigger_t action, void *ctx)
+{
+	struct lite_buttons_config const *config = button_get(pin);
+	bool is_set = nrfx_gpiote_in_is_set(config->pin_number);
+	bool is_active = !((config->active_state == LITE_BUTTONS_ACTIVE_HIGH) ^ is_set);
+
+	/* If event indicates that pin is active and no other pin is active start the timer. All
+	 * action happens in timeout event.
+	 */
+	if (is_active && (global.pin_active == 0)) {
+		LOG_DBG("First active button, starting periodic timer");
+		timer_start();
+	}
+}
+
+int lite_buttons_init(struct lite_buttons_config const *configs, uint8_t num_configs,
+		      uint32_t detection_delay)
+{
+	int err;
+
+	if (global.is_init) {
+		return -EPERM;
+	}
+
+	if (!configs) {
+		return -EINVAL;
+	}
+
+	/* todo: define limit somewhere (lite_timer?). */
+	if (detection_delay < 2 * LITE_TIMER_MIN_TIMEOUT_US) {
+		return -EINVAL;
+	}
+
+	if (!nrfx_gpiote_init_check(&gpio_instance)) {
+		err = nrfx_gpiote_init(&gpio_instance, 0);
+		if (err != NRFX_SUCCESS) {
+			LOG_ERR("Failed to initialize gpiote, err: 0x%08X", err);
+			return -EIO;
+		}
+
+		IRQ_CONNECT(NRFX_IRQ_NUMBER_GET(NRF_GPIOTE_INST_GET(GPIOTE_INST)), IRQ_PRIO,
+			    NRFX_GPIOTE_INST_HANDLER_GET(GPIOTE_INST), 0, 0);
+	}
+
+	global.configs = configs;
+	global.num_configs = num_configs;
+	global.detection_delay = detection_delay;
+
+	const nrfx_gpiote_trigger_config_t trigger_config = {
+		.trigger = NRFX_GPIOTE_TRIGGER_HITOLO,
+	};
+
+	const nrfx_gpiote_handler_config_t handler_config = {
+		.handler = gpiote_event_handler,
+	};
+
+	for (int i = 0; i < num_configs; i++) {
+		const nrf_gpio_pin_pull_t pull_config = configs[i].pull_config;
+		const nrfx_gpiote_input_pin_config_t input_config = {
+			.p_pull_config = &pull_config,
+			.p_trigger_config = &trigger_config,
+			.p_handler_config = &handler_config,
+		};
+
+		err = nrfx_gpiote_input_configure(&gpio_instance, configs[i].pin_number,
+						  &input_config);
+		if (err != NRFX_SUCCESS) {
+			LOG_ERR("nrfx_gpiote_input_configure, err: 0x%08X", err);
+			return -EIO;
+		}
+	}
+
+	err = lite_timer_init(&global.timer, LITE_TIMER_MODE_SINGLE_SHOT,
+			      detection_delay_timeout_handler);
+	if (err) {
+		LOG_ERR("lite_timer_init failed, err: %d", err);
+		return -EIO;
+	}
+
+	global.is_init = true;
+
+	return 0;
+}
+
+int lite_buttons_deinit(void)
+{
+	int err;
+
+	if (!global.is_init) {
+		return -EPERM;
+	}
+
+	err = buttons_disable();
+	if (err) {
+		return -EIO;
+	}
+
+	nrfx_gpiote_uninit(&gpio_instance);
+
+	memset(&global, 0, sizeof(global));
+
+	global.is_init = false;
+
+	return 0;
+}
+
+int lite_buttons_enable(void)
+{
+	if (!global.is_init) {
+		return -EPERM;
+	}
+
+	for (int i = 0; i < global.num_configs; i++) {
+		nrfx_gpiote_trigger_enable(&gpio_instance, global.configs[i].pin_number, true);
+	}
+
+	return 0;
+}
+
+int lite_buttons_disable(void)
+{
+	int err;
+
+	if (!global.is_init) {
+		return -EPERM;
+	}
+
+	err = buttons_disable();
+	if (err) {
+		return err;
+	}
+
+	return 0;
+}
+
+bool lite_buttons_is_pressed(uint8_t pin)
+{
+	if (!global.is_init) {
+		return false;
+	}
+
+	struct lite_buttons_config const *config = button_get(pin);
+	if (config) {
+		bool is_set = nrfx_gpiote_in_is_set(config->pin_number);
+
+		return !(is_set ^ (config->active_state == LITE_BUTTONS_ACTIVE_HIGH));
+	}
+
+	return false;
+}
