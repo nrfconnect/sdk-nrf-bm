83f165eb84b2f72b4be80d287cff2f861c2ec96f
Andreas Moltumyr <andreas.moltumyr@nordicsemi.no>
2025-03-19 10:55:22 +0100

    samples: timer: build with and without softdevice enabled in ci
    Build with board overlays by default. Add conf overlay for
    building with softdevice enabled. Add build variant in sample.yaml
    using the softdevice conf overlay.
    
    Enable softdevice at the start and disable it at the end of the
    sample if CONFIG_SOFTDEVICE is set.
    
    Signed-off-by: Andreas Moltumyr <andreas.moltumyr@nordicsemi.no>

--- samples/timer/src/main.c
+++ samples/timer/src/main.c
@@ -7,6 +7,11 @@
 #include <lite_timer.h>
 #include <zephyr/sys/printk.h>
 
+#if CONFIG_SOFTDEVICE
+#include <nrf_sdh.h>
+#include <nrf_sdh_ble.h>
+#endif /* CONFIG_SOFTDEVICE */
+
 #define PERIODIC_TIMER_TICKS LITE_TIMER_MS_TO_TICKS(CONFIG_PERIODIC_TIMER_INTERVAL_MS)
 #define HELLO_TIMER_TICKS    LITE_TIMER_MS_TO_TICKS(CONFIG_HELLO_TIMER_DURATION_MS)
 #define WORLD_TIMER_TICKS    LITE_TIMER_MS_TO_TICKS(CONFIG_WORLD_TIMER_DURATION_MS)
@@ -71,6 +76,33 @@ int main(void)
 {
 	int err;
 
+#if CONFIG_SOFTDEVICE
+#define CONN_TAG 1
+	uint32_t ram_start;
+
+	err = nrf_sdh_enable_request();
+	if (err) {
+		printk("Failed to enable SoftDevice, err %d\n", err);
+		return -1;
+	}
+
+	printk("SoftDevice enabled\n");
+
+	nrf_sdh_ble_app_ram_start_get(&ram_start);
+
+	err = nrf_sdh_ble_default_cfg_set(CONN_TAG);
+	if (err) {
+		printk("Failed to setup default configuration, err %d\n", err);
+		return -1;
+	}
+
+	err = nrf_sdh_ble_enable(ram_start);
+	if (err) {
+		printk("Failed to enable BLE, err %d\n", err);
+		return -1;
+	}
+#endif /* CONFIG_SOFTDEVICE */
+
 	err = lite_timer_init(&periodic_timer, LITE_TIMER_MODE_REPEATED, periodic_timeout_handler);
 	if (err) {
 		printk("Failed to initialize periodic timer, err %d\n", err);
@@ -101,6 +133,16 @@ int main(void)
 		/* Sleep */
 		__WFE();
 	}
+
+#if CONFIG_SOFTDEVICE
+	err = nrf_sdh_disable_request();
+	if (err) {
+		printk("Failed to disable SoftDevice, err %d\n", err);
+		return -1;
+	}
+
+	printk("SoftDevice disabled\n");
+#endif /* CONFIG_SOFTDEVICE */
 
 	return 0;
 }
