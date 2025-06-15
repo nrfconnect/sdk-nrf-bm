9a4d31a6c70ea0d17d5e1c81add888b78432a643
Emanuele Di Santo <emdi@nordicsemi.no>
2025-06-15 15:52:32 +0200

    lib: bm_queue: a dynamic queue implementation
    A dynamic queue implementation based on Zephyr's k_queue.
    The library has a compatibility mode to export Zephyr's k_queue API,
    but it also exposes its own bm_queue API set so that bare metal
    libraries and application can avoid including zephyr/kernel.h
    and using a k_ prefixed (kernel's) API.
    
    Signed-off-by: Emanuele Di Santo <emdi@nordicsemi.no>

--- samples/bluetooth/hello_softdevice/src/main.c
+++ samples/bluetooth/hello_softdevice/src/main.c
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
+#include <string.h>
 #include <nrf_sdh.h>
 #include <nrf_sdh_ble.h>
 #include <nrf_sdh_soc.h>
@@ -11,6 +12,12 @@
 #include <zephyr/sys_clock.h> /* USEC_PER_SEC */
 #include <zephyr/logging/log_ctrl.h>
 #include <zephyr/sys/printk.h>
+#include <bm_queue.h>
+
+struct item {
+	sys_slist_t reserved;
+	int foo;
+};
 
 static void on_ble_evt(const ble_evt_t *evt, void *ctx)
 {
@@ -29,6 +36,9 @@ static void on_state_change(enum nrf_sdh_state_evt state, void *ctx)
 	printk("SoftDevice state has changed to %d\n", state);
 }
 NRF_SDH_STATE_EVT_OBSERVER(sdh_state, on_state_change, NULL, 0);
+
+#include <nrf.h>
+#include <sys/types.h>
 
 int main(void)
 {
@@ -70,6 +80,43 @@ int main(void)
 	while (LOG_PROCESS()) {
 		/* Empty. */
 	}
+
+	struct k_queue k;
+	struct item *element;
+	struct item i = {
+		.foo = 0xDEADBEEF,
+	};
+	bool empty;
+
+	k_queue_init(&k);
+	element = k_queue_peek_head(&k);
+	printk("Head is %p\n", element);
+	empty = k_queue_is_empty(&k);
+	if (empty) {
+		printk("Queue is empty\n");
+	} else {
+		printk("Queue is not empty\n");
+	}
+
+	k_queue_append(&k, &i);
+	while (!k_queue_is_empty(&k)) {
+		element = k_queue_get(&k, K_NO_WAIT);
+		printk("Element is %x\n", element->foo);
+	}
+
+#if DONTCOMPILE
+	k_queue_prepend(&k, &i);
+	k_queue_alloc_append(&k, &i);
+	k_queue_alloc_prepend(&k, &i);
+	k_queue_append_list(&k, NULL, NULL);
+	k_queue_merge_slist(&k, NULL);
+	k_queue_get(&k, K_NO_WAIT);
+	k_queue_remove(&k, &i);
+	k_queue_unique_append(&k, &i);
+	k_queue_peek_head(&k);
+	k_queue_peek_tail(&k);
+	k_queue_is_empty(&k);
+#endif
 
 	return 0;
 }
