90bde048103928da292a8b356c43680220fcbb98
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2025-06-20 08:59:52 +0100

    samples: Add UART MCUmgr sample application
    Adds a sample application which handles MCUmgr requests for a
    firmware loader image
    
    Files taken from zephyr commit:
      0fe59bf1e4b96122c3467295b09a034e399c5ee6
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

---
+++ samples/mcumgr/uart_mcumgr/include/smp_uart.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2025 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+/**
+ * @brief Processes UART SMP (MCUmgr) receive queue
+ */
+void smp_uart_process_rx_queue(void);
