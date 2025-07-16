c69624ef11146b3eb7ea8dc7d112793990203548
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2025-07-16 14:41:46 +0100

    settings: Add bluetooth name subsystem
    Adds a settings subsystem file to add a bluetooth name setting
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>
    Signed-off-by: Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>

---
+++ subsys/settings/src/bluetooth_name.c
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2025 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/settings/settings.h>
+#include <zephyr/logging/log.h>
+#include <bm/settings/bluetooth_name.h>
+
+LOG_MODULE_REGISTER(settings_bluetooth_name, CONFIG_SETTINGS_LOG_LEVEL);
+
+static char bluetooth_name_val[16] = "";
+
+static int bluetooth_name_handle_set(const char *name, size_t len, settings_read_cb read_cb,
+				     void *cb_arg);
+static int bluetooth_name_handle_export(int (*cb)(const char *name, const void *value,
+					size_t val_len));
+static int bluetooth_name_handle_commit(void);
+static int bluetooth_name_handle_get(const char *name, char *val, int val_len_max);
+
+SETTINGS_STATIC_HANDLER_DEFINE(bluetooth_name, "fw_loader", bluetooth_name_handle_get,
+			       bluetooth_name_handle_set, bluetooth_name_handle_commit,
+			       bluetooth_name_handle_export);
+
+static int bluetooth_name_handle_set(const char *name, size_t len, settings_read_cb read_cb,
+				     void *cb_arg)
+{
+	const char *next;
+	size_t name_len;
+	int rc;
+
+	name_len = settings_name_next(name, &next);
+
+	if (!next) {
+		if (!strncmp(name, "adv_name", name_len)) {
+			if (len > (sizeof(bluetooth_name_val) - 1)) {
+				LOG_ERR("Invalid settings value for fw_loader/adv_name");
+				return -EINVAL;
+			}
+
+			rc = read_cb(cb_arg, bluetooth_name_val, sizeof(bluetooth_name_val));
+
+			if (rc < 0) {
+				return rc;
+			} else if (rc > 0) {
+				LOG_INF("Config set to %s", bluetooth_name_val);
+			}
+
+			return 0;
+		}
+	}
+
+	return -ENOENT;
+}
+
+static int bluetooth_name_handle_export(int (*cb)(const char *name, const void *value,
+					size_t val_len))
+{
+	(void)cb("fw_loader/adv_name", bluetooth_name_val, strlen(bluetooth_name_val) + 1);
+	LOG_INF("export_done");
+	return 0;
+}
+
+static int bluetooth_name_handle_commit(void)
+{
+	LOG_INF("loading_done");
+	return 0;
+}
+
+static int bluetooth_name_handle_get(const char *name, char *val, int val_len_max)
+{
+	const char *next;
+
+	if (name == NULL || val == NULL) {
+		return -EINVAL;
+	}
+
+	if (settings_name_steq(name, "adv_name", &next) && !next) {
+		val_len_max = MIN(val_len_max, strlen(bluetooth_name_val));
+		memcpy(val, bluetooth_name_val, val_len_max);
+		return val_len_max;
+	}
+
+	return -ENOENT;
+}
+
+const char *bluetooth_name_value_get(void)
+{
+	return bluetooth_name_val;
+}
