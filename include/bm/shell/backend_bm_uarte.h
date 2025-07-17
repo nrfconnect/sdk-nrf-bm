d50db68f375b1730f69d7324ede3b6460dbc1844
Bjarki Arge Andreasen <bjarki.andreasen@nordicsemi.no>
2025-07-17 12:49:44 +0200

    shell: introduce shell support using bm uarte shell backend
    Implement shell support using zephyr's shell subsys with slight
    modifications to allow it to be used single threaded, and add nrfx
    uarte based shell backend for the shell to use.
    
    Signed-off-by: Bjarki Arge Andreasen <bjarki.andreasen@nordicsemi.no>

---
+++ include/bm/shell/backend_bm_uarte.h
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2026 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+/* Get pointer to BM UARTE shell backend */
+const struct shell *shell_backend_bm_uarte_get_ptr(void);
