7af962cd97dd31592cb63a62194bbfd789a26ea8
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2025-08-13 12:14:28 +0100

    samples: boot: mcuboot_recovery_entry: Fix sample
    Fixes sample so that it works and receives/responds to MCUmgr
    messages, also refactors code so that OS mgmt reset response is
    not lost
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

--- samples/boot/mcuboot_recovery_entry/src/main.c
+++ samples/boot/mcuboot_recovery_entry/src/main.c
@@ -4,19 +4,131 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
-#include <zephyr/sys/reboot.h>
-#include <zephyr/retention/bootmode.h>
-#include <zephyr/kernel.h>
-#include <zephyr/logging/log_ctrl.h>
-
 #include <nrf_sdh.h>
 #include <ble_adv.h>
-#include <bluetooth/services/ble_mcumgr.h>
+#include <zephyr/kernel.h>
+#include <zephyr/sys/reboot.h>
+#include <zephyr/retention/bootmode.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/logging/log_ctrl.h>
 #include <zephyr/mgmt/mcumgr/mgmt/callbacks.h>
+#include <bluetooth/services/ble_mcumgr.h>
+
+LOG_MODULE_REGISTER(app, CONFIG_APP_LOG_LEVEL);
 
 BLE_ADV_DEF(ble_adv); /* BLE advertising instance */
 
-static bool should_reboot;
+/** Handle of the current connection */
+static uint16_t conn_handle = BLE_CONN_HANDLE_INVALID;
+static volatile bool should_reboot;
+static volatile bool notification_sent;
+static volatile bool device_disconnected;
+
+static enum mgmt_cb_return os_mgmt_reboot_hook(uint32_t event, enum mgmt_cb_return prev_status,
+					       int32_t *rc, uint16_t *group, bool *abort_more,
+					       void *data, size_t data_size);
+
+static struct mgmt_callback os_mgmt_reboot_callback = {
+	.callback = os_mgmt_reboot_hook,
+	.event_id = MGMT_EVT_OP_OS_MGMT_RESET,
+};
+
+/**
+ * @brief BLE event handler.
+ *
+ * @param[in] evt BLE event.
+ * @param[in] ctx Context.
+ */
+static void on_ble_evt(const ble_evt_t *evt, void *ctx)
+{
+	int err;
+
+	__ASSERT(ble_evt, "BLE event is NULL");
+
+	if (evt == NULL) {
+		return;
+	}
+
+	switch (evt->header.evt_id) {
+	case BLE_GAP_EVT_CONNECTED:
+	{
+		LOG_INF("Peer connected");
+		conn_handle = evt->evt.gap_evt.conn_handle;
+		err = sd_ble_gatts_sys_attr_set(evt->evt.gap_evt.conn_handle, NULL, 0, 0);
+
+		if (err) {
+			LOG_ERR("Failed to set system attributes, nrf_error %#x", err);
+		}
+		break;
+	}
+
+	case BLE_GAP_EVT_DISCONNECTED:
+	{
+		LOG_INF("Peer disconnected");
+		conn_handle = BLE_CONN_HANDLE_INVALID;
+
+		if (should_reboot == true) {
+			device_disconnected = true;
+		}
+		break;
+	}
+
+	case BLE_GAP_EVT_AUTH_STATUS:
+	{
+		LOG_INF("Authentication status: %#x",
+			evt->evt.gap_evt.params.auth_status.auth_status);
+		break;
+	}
+
+	case BLE_GAP_EVT_SEC_PARAMS_REQUEST:
+	{
+		/* Pairing not supported */
+		err = sd_ble_gap_sec_params_reply(evt->evt.gap_evt.conn_handle,
+						  BLE_GAP_SEC_STATUS_PAIRING_NOT_SUPP, NULL, NULL);
+
+		if (err) {
+			LOG_ERR("Failed to reply with Security params, nrf_error %#x", err);
+		}
+
+		break;
+	}
+
+	case BLE_GATTS_EVT_SYS_ATTR_MISSING:
+	{
+		LOG_INF("BLE_GATTS_EVT_SYS_ATTR_MISSING");
+		/* No system attributes have been stored */
+		err = sd_ble_gatts_sys_attr_set(evt->evt.gap_evt.conn_handle, NULL, 0, 0);
+
+		if (err) {
+			LOG_ERR("Failed to set system attributes, nrf_error %#x", err);
+		}
+
+		break;
+	}
+
+	case BLE_GATTS_EVT_HVN_TX_COMPLETE:
+	{
+		if (should_reboot == true) {
+			notification_sent = true;
+		}
+
+		break;
+	}
+	};
+}
+
+NRF_SDH_BLE_OBSERVER(sdh_ble, on_ble_evt, NULL, 0);
+
+static void ble_adv_evt_handler(struct ble_adv *adv, const struct ble_adv_evt *adv_evt)
+{
+	switch (adv_evt->evt_type) {
+	case BLE_ADV_EVT_ERROR:
+		printk("Advertising error %d\n", adv_evt->error.reason);
+		break;
+	default:
+		break;
+	}
+}
 
 static enum mgmt_cb_return os_mgmt_reboot_hook(uint32_t event, enum mgmt_cb_return prev_status,
 					       int32_t *rc, uint16_t *group, bool *abort_more,
@@ -33,22 +145,6 @@ static enum mgmt_cb_return os_mgmt_reboot_hook(uint32_t event, enum mgmt_cb_retu
 	}
 
 	return MGMT_CB_OK;
-}
-
-static struct mgmt_callback os_mgmt_reboot_callback = {
-	.callback = os_mgmt_reboot_hook,
-	.event_id = MGMT_EVT_OP_OS_MGMT_RESET,
-};
-
-static void ble_adv_evt_handler(struct ble_adv *adv, const struct ble_adv_evt *adv_evt)
-{
-	switch (adv_evt->evt_type) {
-	case BLE_ADV_EVT_ERROR:
-		printk("Advertising error %d\n", adv_evt->error.reason);
-		break;
-	default:
-		break;
-	}
 }
 
 int main(void)
@@ -114,7 +210,7 @@ int main(void)
 
 	printk("Advertising as %s\n", CONFIG_BLE_ADV_NAME);
 
-	while (should_reboot == false) {
+	while (notification_sent == false && device_disconnected == false) {
 		while (LOG_PROCESS()) {
 		}
 
@@ -124,6 +220,27 @@ int main(void)
 		/* Clear Event Register */
 		__SEV();
 		__WFE();
+	}
+
+	if (device_disconnected == false) {
+		err = sd_ble_gap_disconnect(conn_handle,
+					    BLE_HCI_REMOTE_USER_TERMINATED_CONNECTION);
+
+		if (err != NRF_SUCCESS) {
+			device_disconnected = true;
+		}
+
+		while (device_disconnected == false) {
+			while (LOG_PROCESS()) {
+			}
+
+			/* Wait for an event. */
+			__WFE();
+
+			/* Clear Event Register */
+			__SEV();
+			__WFE();
+		}
 	}
 
 	sys_reboot(SYS_REBOOT_WARM);
