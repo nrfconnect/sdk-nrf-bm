4b752adb9d58720cf427556981c87ff338aec201
Eivind Jølsgard <eivind.jolsgard@nordicsemi.no>
2025-08-13 12:07:39 +0200

    samples: bluetooth: hello_softdevice: use logs instead of printk
    Use logs instead of printk in the sample.
    
    Signed-off-by: Eivind Jølsgard <eivind.jolsgard@nordicsemi.no>

--- samples/bluetooth/hello_softdevice/src/main.c
+++ samples/bluetooth/hello_softdevice/src/main.c
@@ -9,24 +9,26 @@
 #include <nrf_sdh_soc.h>
 #include <zephyr/kernel.h> /* k_busy_wait() */
 #include <zephyr/sys_clock.h> /* USEC_PER_SEC */
+#include <zephyr/logging/log.h>
 #include <zephyr/logging/log_ctrl.h>
-#include <zephyr/sys/printk.h>
+
+LOG_MODULE_REGISTER(app, CONFIG_BLE_HELLO_SD_SAMPLE_LOG_LEVEL);
 
 static void on_ble_evt(const ble_evt_t *evt, void *ctx)
 {
-	printk("BLE event %d\n", evt->header.evt_id);
+	LOG_INF("BLE event %d", evt->header.evt_id);
 }
 NRF_SDH_BLE_OBSERVER(sdh_ble, on_ble_evt, NULL, 0);
 
 static void on_soc_evt(uint32_t evt, void *ctx)
 {
-	printk("SoC event\n");
+	LOG_INF("SoC event");
 }
 NRF_SDH_SOC_OBSERVER(sdh_soc, on_soc_evt, NULL, 0);
 
 static void on_state_change(enum nrf_sdh_state_evt state, void *ctx)
 {
-	printk("SoftDevice state has changed to %d\n", state);
+	LOG_INF("SoftDevice state has changed to %d", state);
 }
 NRF_SDH_STATE_EVT_OBSERVER(sdh_state, on_state_change, NULL, 0);
 
@@ -34,23 +36,23 @@ int main(void)
 {
 	int err;
 
-	printk("Hello SoftDevice sample started\n");
+	LOG_INF("Hello SoftDevice sample started");
 
 	err = nrf_sdh_enable_request();
 	if (err) {
-		printk("Failed to enable SoftDevice, err %d\n", err);
+		LOG_ERR("Failed to enable SoftDevice, err %d", err);
 		goto idle;
 	}
 
-	printk("SoftDevice enabled\n");
+	LOG_INF("SoftDevice enabled");
 
 	err = nrf_sdh_ble_enable(CONFIG_NRF_SDH_BLE_CONN_TAG);
 	if (err) {
-		printk("Failed to enable BLE, err %d\n", err);
+		LOG_ERR("Failed to enable BLE, err %d", err);
 		goto idle;
 	}
 
-	printk("Bluetooth enabled\n");
+	LOG_INF("Bluetooth enabled");
 
 	while (LOG_PROCESS()) {
 	}
@@ -59,12 +61,12 @@ int main(void)
 
 	err = nrf_sdh_disable_request();
 	if (err) {
-		printk("Failed to disable SoftDevice, err %d\n", err);
+		LOG_ERR("Failed to disable SoftDevice, err %d", err);
 		goto idle;
 	}
 
-	printk("SoftDevice disabled\n");
-	printk("Bye\n");
+	LOG_INF("SoftDevice disabled");
+	LOG_INF("Bye");
 
 idle:
 	while (true) {
