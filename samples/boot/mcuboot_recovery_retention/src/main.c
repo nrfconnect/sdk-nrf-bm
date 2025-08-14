f489ea539fe45d59ba47ae89663b7a43f298fbf9
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2025-08-14 12:16:13 +0100

    samples: boot: mcuboot_recovery_retention: Switch to logging
    Switches printf to logging functions
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

--- samples/boot/mcuboot_recovery_retention/src/main.c
+++ samples/boot/mcuboot_recovery_retention/src/main.c
@@ -5,16 +5,18 @@
  */
 
 #include <stdint.h>
+#include <zephyr/kernel.h>
 #include <zephyr/sys/reboot.h>
 #include <zephyr/retention/bootmode.h>
-#include <zephyr/kernel.h>
-#include <stdio.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(app, CONFIG_APP_LOG_LEVEL);
 
 int main(void)
 {
 	int rc;
 
-	printf("Waiting...\n");
+	LOG_INF("Waiting...");
 	k_sleep(K_SECONDS(3));
 
 	rc = bootmode_set(BOOT_MODE_TYPE_BOOTLOADER);
@@ -22,7 +24,7 @@ int main(void)
 	if (rc == 0) {
 		sys_reboot(SYS_REBOOT_WARM);
 	} else {
-		printf("Error, failed to set boot mode: %d\n", rc);
+		LOG_INF("Error, failed to set boot mode: %d", rc);
 	}
 
 	return 0;
