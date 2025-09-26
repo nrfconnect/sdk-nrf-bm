ff28882e415c7f1090c8ecb2b714a02065c9bcc4
Bartosz Gentkowski <bartosz.gentkowski@nordicsemi.no>
2025-09-26 14:53:19 +0200

    doc: update doc after 0.9.0
    change version to 0.9.99 and bring back changelog.
    
    Signed-off-by: Bartosz Gentkowski <bartosz.gentkowski@nordicsemi.no>

--- doc/nrf-bm/conf.py
+++ doc/nrf-bm/conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'nRF Connect SDK Bare Metal option - 0.9.0'
+project = 'nRF Connect SDK Bare Metal option - 0.9.99'
 copyright = '2025, Nordic Semiconductor'
 author = 'Nordic Semiconductor'
 
