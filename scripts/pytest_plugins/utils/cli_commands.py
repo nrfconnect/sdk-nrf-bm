3bc9a3d67aa5e8cd77bffa2a7c7335b1b5b67c45
Lukasz Fundakowski <lukasz.fundakowski@nordicsemi.no>
2025-11-12 10:26:48 +0100

    tests: Add tests for keys revocation
    Added twister tests for keys revocation in mcuboot.
    
    Signed-off-by: Lukasz Fundakowski <lukasz.fundakowski@nordicsemi.no>

--- scripts/pytest_plugins/utils/cli_commands.py
+++ scripts/pytest_plugins/utils/cli_commands.py
@@ -2,14 +2,31 @@
 #
 # SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 
+import abc
+import concurrent.futures
 import logging
+import multiprocessing
 import os
 import shlex
+import shutil
 import subprocess
+import time
+from collections.abc import Callable, Sequence
+from dataclasses import dataclass
 from pathlib import Path
-from typing import Literal
+from typing import Any, Literal
+
+MAX_WORKERS = max(multiprocessing.cpu_count() - 1, 1)
 
 logger = logging.getLogger(__name__)
+
+
+class WestFlashException(Exception):
+    """Raised when west flash failed."""
+
+
+class WestBuildException(Exception):
+    """Raised when west build failed."""
 
 
 def normalize_path(path: str) -> str:
@@ -18,7 +35,25 @@ def normalize_path(path: str) -> str:
     return path
 
 
+def duration(func: Callable) -> Any:
+    """Decorator to print duration of calling a function."""
+
+    def _inner(*args, **kwargs):
+        start_t = time.monotonic()
+        try:
+            return func(*args, **kwargs)
+        finally:
+            logger.debug(
+                "The `%s` function took %s seconds to execute",
+                func.__name__,
+                round(time.monotonic() - start_t, 3),
+            )
+
+    return _inner
+
+
 def run_command(command: list[str], timeout: int = 30) -> None:
+    """Run command in subprocess."""
     logger.info(f"CMD: {shlex.join(command)}")
     ret: subprocess.CompletedProcess = subprocess.run(
         command,
@@ -73,3 +108,94 @@ def provision_keys_for_kmu(
 
     run_command(command)
     logger.info("Keys provisioned successfully")
+
+
+class Builder(abc.ABC):
+    """Interface class for builders."""
+
+    @abc.abstractmethod
+    def build(self) -> tuple[Path, Path]:
+        """Run build and return source and build paths as tuple."""
+
+
+@dataclass
+class WestBuilder(Builder):
+    source_dir: Path
+    build_dir: Path
+    board: str
+    testsuite: str | None = None
+    extra_args: str | None = None
+    timeout: int = 60
+
+    @duration
+    def build(self) -> tuple[Path, Path]:
+        """Run west build for the required build configuration."""
+        command = [
+            "west",
+            "build",
+            "-p",
+            "-b",
+            self.board,
+            str(self.source_dir),
+            "-d",
+            str(self.build_dir),
+        ]
+        if self.testsuite:
+            command.extend(["-T", self.testsuite])
+        if self.extra_args:
+            command.extend(self.extra_args.split(" "))
+
+        try:
+            run_command(command, timeout=self.timeout)
+        except subprocess.CalledProcessError:
+            # create empty file to indicate a build error, will be used
+            # to avoid unnecessary builds in other tests
+            Path(self.build_dir / "build.error").touch()
+            raise WestBuildException("Failed to build required app") from None
+        except subprocess.TimeoutExpired:
+            logger.error("Timeout building required app")
+            shutil.rmtree(self.build_dir)
+            raise WestBuildException("Timeout building required app") from None
+
+        return self.source_dir, self.build_dir
+
+
+class BuildDirector:
+    """Orchestrator for builders to build in paraller."""
+
+    def __init__(self, builders: Sequence[Builder], max_workers: int = MAX_WORKERS) -> None:
+        self.builders = builders
+        self.max_workers = max_workers
+        self.excptions: list[Exception] = []
+
+    def run(self):
+        with concurrent.futures.ThreadPoolExecutor(max_workers=self.max_workers) as executor:
+            tasks = {executor.submit(builder.build) for builder in self.builders}
+            for future in concurrent.futures.as_completed(tasks):
+                try:
+                    _, build_dir = future.result()
+                except Exception as exc:
+                    logger.error('Failed to build: %s', exc)
+                    self.excptions.append(exc)
+                else:
+                    logger.info("Build success: %s", build_dir)
+        logger.info("Finished building samples")
+
+
+def west_flash(
+    build_dir: Path, snr: str | None, *, extra_args: str | None = None, timeout: int = 30
+) -> None:
+    """Run west flash."""
+    command = ["west", "flash", "--skip-rebuild", "--build-dir", str(build_dir)]
+    if snr:
+        command += ["--dev-id", snr]
+    if extra_args:
+        command.extend(extra_args.split(" "))
+
+    try:
+        run_command(command, timeout=timeout)
+    except subprocess.CalledProcessError:
+        raise WestFlashException("Failed to flash device") from None
+    except subprocess.TimeoutExpired:
+        logger.error("Timeout flaashing device")
+        raise WestFlashException("Timeout flaashing device") from None
---
+++ tests/subsys/kmu/pytest/test_kmu_key_revocation.py
@@ -0,0 +1,282 @@
+# Copyright (c) 2025 Nordic Semiconductor ASA
+#
+# SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+
+from collections.abc import Callable
+from pathlib import Path
+
+import pytest
+from pytest_plugins.utils.cli_commands import (
+    BuildDirector,
+    WestBuilder,
+    provision_keys_for_kmu,
+    reset_board,
+    west_flash,
+)
+from twister_harness import DeviceAdapter
+
+KMU_ROOT: Path = Path(__file__).parents[1]
+
+LINES_FOR_CORRECT_BOOT = [
+    "*Starting bootloader*",
+    "*Booting main application*",
+    "*Jumping to the first image slot*",
+    "*Hello World*",
+]
+LINES_FOR_REVOCED_KEYS = [
+    "*Starting bootloader*",
+    "*ED25519 signature verification failed -140*",
+    "*Error: no bootable configuration found*",
+    "*Unable to find bootable image*",
+]
+LINES_FOR_KEY_VERIFICATION_FAIL = [
+    "*Starting bootloader*",
+    "*ED25519 signature verification failed -149*",
+    "*Error: no bootable configuration found*",
+    "*Unable to find bootable image*",
+]
+
+
+@pytest.mark.usefixtures("no_reset")
+@pytest.mark.parametrize(
+    "keys_order",
+    [
+        ("valid",),
+        ("valid", "valid", "valid"),
+        ("valid", "invalid-1", "invalid-2"),
+        ("invalid-1", "valid", "invalid-2"),
+        ("invalid-1", "invalid-2", "valid"),
+    ],
+    ids=("one-valid-key", "all-valid", "first-valid", "second-valid", "third-valid"),
+)
+def test_if_kmu_keys_revocation_with_three_slots(
+    dut: DeviceAdapter, config_reader: Callable, nrf_bm_path: Path, keys_order: tuple[str, ...]
+):
+    """Verify if KMU keys are provisioned correctly."""
+    sysbuild_config = Path(dut.device_config.build_dir) / "zephyr" / ".config"
+    valid_key_file = config_reader(sysbuild_config).read(
+        "SB_CONFIG_BM_BOOTLOADER_MCUBOOT_SIGNATURE_KEY_FILE"
+    )
+    keys_dict = {
+        "valid": valid_key_file,
+        "invalid-1": nrf_bm_path / "tests/subsys/kmu/keys/ed25519-2.pem",
+        "invalid-2": nrf_bm_path / "tests/subsys/kmu/keys/ed25519-3.pem",
+    }
+    keys = [keys_dict[key] for key in keys_order]
+
+    provision_keys_for_kmu(keys=keys, keyname="BL_PUBKEY", dev_id=dut.device_config.id)
+    dut.clear_buffer()
+    reset_board(dut.device_config.id)
+
+    lines = dut.readlines_until(regex="Hello World!", print_output=True, timeout=20)
+    pytest.LineMatcher(lines).fnmatch_lines(LINES_FOR_CORRECT_BOOT)
+
+
+@pytest.mark.usefixtures("no_reset")
+@pytest.mark.parametrize(
+    "keys_order",
+    [
+        ("valid", "invalid-1", "invalid-2"),
+        ("invalid-1", "valid", "invalid-2"),
+    ],
+    ids=("first-valid", "second-valid"),
+)
+def test_if_kmu_keys_revocation_with_two_slots(
+    dut: DeviceAdapter, config_reader: Callable, nrf_bm_path: Path, keys_order: tuple[str, ...]
+):
+    """Verify if KMU keys are provisioned correctly."""
+    sysbuild_config = Path(dut.device_config.build_dir) / "zephyr" / ".config"
+    valid_key_file = config_reader(sysbuild_config).read(
+        "SB_CONFIG_BM_BOOTLOADER_MCUBOOT_SIGNATURE_KEY_FILE"
+    )
+    keys_dict = {
+        "valid": valid_key_file,
+        "invalid-1": nrf_bm_path / "tests/subsys/kmu/keys/ed25519-2.pem",
+        "invalid-2": nrf_bm_path / "tests/subsys/kmu/keys/ed25519-3.pem",
+    }
+    keys = [keys_dict[key] for key in keys_order]
+
+    provision_keys_for_kmu(keys=keys, keyname="BL_PUBKEY", dev_id=dut.device_config.id)
+    dut.clear_buffer()
+    reset_board(dut.device_config.id)
+
+    lines = dut.readlines_until(regex="Hello World!", print_output=True, timeout=20)
+    pytest.LineMatcher(lines).fnmatch_lines(LINES_FOR_CORRECT_BOOT)
+
+
+@pytest.mark.usefixtures("no_reset")
+def test_if_kmu_keys_revocation_with_two_slots_third_key_valid(
+    dut: DeviceAdapter, config_reader: Callable, nrf_bm_path: Path
+):
+    """Verify if KMU keys are provisioned correctly."""
+    sysbuild_config = Path(dut.device_config.build_dir) / "zephyr" / ".config"
+    valid_key_file = config_reader(sysbuild_config).read(
+        "SB_CONFIG_BM_BOOTLOADER_MCUBOOT_SIGNATURE_KEY_FILE"
+    )
+    keys_dict = {
+        "valid": valid_key_file,
+        "invalid-1": nrf_bm_path / "tests/subsys/kmu/keys/ed25519-2.pem",
+        "invalid-2": nrf_bm_path / "tests/subsys/kmu/keys/ed25519-3.pem",
+    }
+    keys_order = ("invalid-1", "invalid-2", "valid")
+    keys = [keys_dict[key] for key in keys_order]
+
+    provision_keys_for_kmu(keys=keys, keyname="BL_PUBKEY", dev_id=dut.device_config.id)
+    dut.clear_buffer()
+    reset_board(dut.device_config.id)
+
+    lines = dut.readlines_until(
+        regex="Unable to find bootable image", print_output=True, timeout=20
+    )
+    pytest.LineMatcher(lines).fnmatch_lines(LINES_FOR_KEY_VERIFICATION_FAIL)
+
+
+@pytest.mark.xfail(reason="Won't work until https://github.com/nrfconnect/sdk-mcuboot/pull/564")
+@pytest.mark.usefixtures("no_reset")
+def test_if_previous_key_is_revoked_when_flashing_new_image(
+    dut: DeviceAdapter, config_reader: Callable, nrf_bm_path: Path, request: pytest.FixtureRequest
+):
+    """Test if previous key is revoked when flashing with new image."""
+    sysbuild_config = Path(dut.device_config.build_dir) / "zephyr" / ".config"
+    valid_key_file = config_reader(sysbuild_config).read(
+        "SB_CONFIG_BM_BOOTLOADER_MCUBOOT_SIGNATURE_KEY_FILE"
+    )
+    keys = {
+        1: valid_key_file,
+        2: nrf_bm_path / "tests/subsys/kmu/keys/ed25519-2.pem",
+        3: nrf_bm_path / "tests/subsys/kmu/keys/ed25519-3.pem",
+    }
+
+    build_dir_1 = dut.device_config.build_dir
+    # build second image signed with different key
+    build_dir_2 = dut.device_config.build_dir.parent / f"{request.node.name}_key2"
+    testsuite: str = dut.device_config.build_dir.name  # "mcuboot.kmu.west.provision.key_revocation"
+    WestBuilder(
+        source_dir=KMU_ROOT / dut.device_config.build_dir.parent.name,
+        build_dir=build_dir_2,
+        board=dut.device_config.platform,
+        testsuite=testsuite,
+        extra_args=f"-DSB_CONFIG_BM_BOOTLOADER_MCUBOOT_SIGNATURE_KEY_FILE=\"{keys[2]}\"",
+        timeout=120,
+    ).build()
+
+    # provision all keys
+    provision_keys_for_kmu(
+        keys=list(keys.values()), keyname="BL_PUBKEY", dev_id=dut.device_config.id
+    )
+    dut.clear_buffer()
+    reset_board(dut.device_config.id)
+
+    lines = dut.readlines_until(regex="Hello World!", print_output=True, timeout=20)
+    pytest.LineMatcher(lines).fnmatch_lines(LINES_FOR_CORRECT_BOOT)
+
+    # flash DUT with the second image
+    west_flash(build_dir_2, dut.device_config.id)
+    dut.clear_buffer()
+    reset_board(dut.device_config.id)
+    lines = dut.readlines_until(regex="Hello World!", print_output=True, timeout=20)
+    pytest.LineMatcher(lines).fnmatch_lines(LINES_FOR_CORRECT_BOOT)
+
+    # flash again with the first image
+    west_flash(build_dir_1, dut.device_config.id)
+    dut.clear_buffer()
+    reset_board(dut.device_config.id)
+
+    # verify that DUT does not boot because the key was revoked in previous step
+    lines = dut.readlines_until(
+        regex="Unable to find bootable image|Hello World!", print_output=True, timeout=20
+    )
+    pytest.LineMatcher(lines).no_fnmatch_line("*Hello World!*")
+    pytest.LineMatcher(lines).fnmatch_lines(LINES_FOR_KEY_VERIFICATION_FAIL)
+
+
+@pytest.mark.usefixtures("no_reset")
+def test_if_revocation_of_last_remaining_key_is_not_allowed(
+    dut: DeviceAdapter, config_reader: Callable, nrf_bm_path: Path, request: pytest.FixtureRequest
+):
+    """Prevent Revocation of Last Remaining Key.
+
+    - build images signed with key1, key2 and key3
+    - provison Dut with 2 keys (key1 and key2)
+    - flash device with the image signed with key1 and reset
+    - flash device with the image signed with key2 and reset
+    - flash device with the image signed with key3 and reset
+    - verified that Dut does not boot due to 3rd image is not signed with valid key
+    - flash device with image sidned with key2
+    - verify that Dut boots correctly and `Hello World` is printed to UART console
+    """
+    sysbuild_config = Path(dut.device_config.build_dir) / "zephyr" / ".config"
+    valid_key_file = config_reader(sysbuild_config).read(
+        "SB_CONFIG_BM_BOOTLOADER_MCUBOOT_SIGNATURE_KEY_FILE"
+    )
+
+    keys = {
+        1: valid_key_file,
+        2: nrf_bm_path / "tests/subsys/kmu/keys/ed25519-2.pem",
+        3: nrf_bm_path / "tests/subsys/kmu/keys/ed25519-3.pem",
+    }
+
+    # build images signed with different keys
+    source_dir = KMU_ROOT / dut.device_config.build_dir.parent.name
+    build_dir_2 = dut.device_config.build_dir.parent / f"{request.node.name}_key2"
+    build_dir_3 = dut.device_config.build_dir.parent / f"{request.node.name}_key3"
+    testsuite: str = "mcuboot.kmu.west.provision.key_revocation"
+    builders = [
+        WestBuilder(
+            source_dir=source_dir,
+            build_dir=build_dir_2,
+            board=dut.device_config.platform,
+            testsuite=testsuite,
+            extra_args=f"-DSB_CONFIG_BM_BOOTLOADER_MCUBOOT_SIGNATURE_KEY_FILE=\"{keys[2]}\"",
+            timeout=120,
+        ),
+        WestBuilder(
+            source_dir=source_dir,
+            build_dir=build_dir_3,
+            board=dut.device_config.platform,
+            testsuite=testsuite,
+            extra_args=f"-DSB_CONFIG_BM_BOOTLOADER_MCUBOOT_SIGNATURE_KEY_FILE=\"{keys[3]}\"",
+            timeout=120,
+        ),
+    ]
+
+    build_director = BuildDirector(builders)
+    build_director.run()
+    assert not build_director.excptions, "Building samples failed"
+
+    # provision with first two keys
+    provision_keys_for_kmu(
+        keys=list(keys.values())[:2], keyname="BL_PUBKEY", dev_id=dut.device_config.id
+    )
+    dut.clear_buffer()
+    reset_board(dut.device_config.id)
+
+    lines = dut.readlines_until(regex="Hello World!", print_output=True, timeout=20)
+    pytest.LineMatcher(lines).fnmatch_lines(LINES_FOR_CORRECT_BOOT)
+
+    # flash DUT with the second image
+    west_flash(build_dir_2, dut.device_config.id)
+    dut.clear_buffer()
+    reset_board(dut.device_config.id)
+    lines = dut.readlines_until(regex="Hello World!", print_output=True, timeout=20)
+    pytest.LineMatcher(lines).fnmatch_lines(LINES_FOR_CORRECT_BOOT)
+
+    # flash DUT with the third image
+    west_flash(build_dir_3, dut.device_config.id)
+    dut.clear_buffer()
+    reset_board(dut.device_config.id)
+    lines = dut.readlines_until(
+        regex="Unable to find bootable image|Hello World!", print_output=True, timeout=20
+    )
+    pytest.LineMatcher(lines).no_fnmatch_line("*Hello World!*")
+    pytest.LineMatcher(lines).fnmatch_lines(LINES_FOR_REVOCED_KEYS)
+
+    # flash DUT with the second image
+    west_flash(build_dir_2, dut.device_config.id)
+    dut.clear_buffer()
+    reset_board(dut.device_config.id)
+    # Check if Dut boots correctly
+    lines = dut.readlines_until(
+        regex="Unable to find bootable image|Hello World!", print_output=True, timeout=20
+    )
+    pytest.LineMatcher(lines).fnmatch_lines(LINES_FOR_CORRECT_BOOT)
