39579a122b9b3c147da32bd8b4b4bb24b27693ca
Andrzej Kuros <andrzej.kuros@nordicsemi.no>
2025-12-05 14:48:17 +0100

    nfc: add TNEP tag signalling implementation for Bare Metal
    The Kconfig NFC_TNEP_TAG_SIGNALLING choice is given the
    NFC_TNEP_TAG_SIGNALLING_BM option for Bare Metal platform.
    This option enables the NFC TNEP tag signalling appropriate for
    Bare Metal platform, where Zephyr primitives are not available.
    
    Signed-off-by: Andrzej Kuros <andrzej.kuros@nordicsemi.no>

---
+++ include/bm/nfc/tnep/tag_signalling_bm.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2026 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef NFC_TNEP_TAG_SIGNALLING_BM_H_
+#define NFC_TNEP_TAG_SIGNALLING_BM_H_
+
+#include <stdint.h>
+
+int nfc_tnep_tag_signalling_init(void);
+
+#endif /* NFC_TNEP_TAG_SIGNALLING_BM_H_ */
