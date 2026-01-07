109f11694a1c17fa74bca5bfa07be020a8463888
Bjørn Tore Taraldsen <bjorn.tore.taraldsen@nordicsemi.no>
2026-01-07 14:06:17 +0100

    boards: Add nRF54LV10A target
    Signed-off-by: Bjørn Tore Taraldsen <bjorn.tore.taraldsen@nordicsemi.no>
    Signed-off-by: Bartosz Gentkowski <bartosz.gentkowski@nordicsemi.no>

---
+++ boards/nordic/bm_nrf54lv10dk/init.c
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2025 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+#include <hal/nrf_regulators.h>
+
+void board_late_init_hook(void)
+{
+	nrf_regulators_vreg_enable_set(NRF_REGULATORS, NRF_REGULATORS_VREG_MAIN, true);
+}
