a94cf1cce452f74e46899566cf666bd9ef3909bd
Eivind Jølsgard <eivind.jolsgard@nordicsemi.no>
2026-01-22 12:20:44 +0100

    include: bm: bluetooth: ble_scan: doxygen and header cleanup
    Cleanup of header and doxygen.
    
    Signed-off-by: Eivind Jølsgard <eivind.jolsgard@nordicsemi.no>

--- include/bm/bluetooth/ble_scan.h
+++ include/bm/bluetooth/ble_scan.h
@@ -11,14 +11,14 @@
  * @{
  * @brief Library for handling the BLE scanning.
  *
- * @details The Scan Library handles the BLE scanning for your application.
- *          The library offers several criteria for filtering the devices available for connection,
- *          and it can also work in the simple mode without using the filtering.
- *          If an event handler is provided, your main application can react to a filter match or to
- *          the need of setting the allow list. The library can also be configured to automatically
- *          connect after it matches a filter or a device from the allow list.
+ * @details The Scan Library offers several criteria for filtering the devices available for
+ *          connection. It can also be used without filtering.
+ *          If an event handler is provided, the library generates an event on a filter match.
+ *          An event is also raised if further configuration is required for use of allow list.
+ *          The library can be configured to automatically connect to a device that matches the
+ *          filter or a device from the allow list.
  *
- * @note    The Scan library also supports applications with a multicentral link.
+ * @note    The Scan library supports applications with a multicentral link.
  */
 
 #ifndef BLE_SCAN_H__
@@ -39,7 +39,6 @@ extern "C" {
  * @brief Macro for defining a ble_scan instance.
  *
  * @param _name Name of the instance.
- * @hideinitializer
  */
 #define BLE_SCAN_DEF(_name)                                                                        \
 	static struct ble_scan _name;                                                              \
@@ -49,25 +48,24 @@ extern "C" {
 /**
  * @brief Scan events.
  *
- * @details These events are propagated to the main application if a handler is provided during the
- * initialization of the Scan library.
+ * @details These events are propagated by the library if a handler is provided as part of the
+ * initialization configuration.
  *
- * @note @ref BLE_SCAN_EVT_ALLOW_LIST_REQUEST cannot be ignored if allow list is used.
+ * @note A @ref BLE_SCAN_EVT_ALLOW_LIST_REQUEST event cannot be ignored if the allow list is used.
  */
 enum ble_scan_evt_type {
 	/**
-	 * @brief A filter is matched or all filters are matched in the multifilter mode.
+	 * @brief A filter is matched, or all filters are matched in the multifilter mode.
 	 */
 	BLE_SCAN_EVT_FILTER_MATCH,
 	/**
-	 * @brief Request the allow list from the main application.
+	 * @brief An allow list must be provided to the SoftDevice.
 	 *
 	 * For allow list scanning to work, the allow list must be set when this event occurs.
 	 */
 	BLE_SCAN_EVT_ALLOW_LIST_REQUEST,
 	/**
-	 * @brief Send notification to the main application when a device from the allow list is
-	 * found.
+	 * @brief A device from the allow list is found.
 	 */
 	BLE_SCAN_EVT_ALLOW_LIST_ADV_REPORT,
 	/**
@@ -79,7 +77,7 @@ enum ble_scan_evt_type {
 	 */
 	BLE_SCAN_EVT_SCAN_TIMEOUT,
 	/**
-	 * @brief Error occurred when establishing the connection.
+	 * @brief An error occurred when establishing the connection.
 	 *
 	 * In this event, an error is passed from the function call @ref sd_ble_gap_connect.
 	 */
@@ -120,7 +118,7 @@ struct ble_scan_short_name {
 	 */
 	const char *short_name;
 	/**
-	 * @brief Minimum length of the short name.
+	 * @brief Minimum length of the short name to be matched.
 	 */
 	uint8_t short_name_min_len;
 };
@@ -144,7 +142,7 @@ struct ble_scan_filter_match {
 /**
  * @brief Scan library event.
  *
- * @details This is used to send library event data to the main application when an event occurs.
+ * @details Event data passed to the main application when an event occurs.
  */
 struct ble_scan_evt {
 	/** Type of event. */
@@ -152,7 +150,7 @@ struct ble_scan_evt {
 	/** GAP scanning parameters. These parameter are needed to establish connection. */
 	const ble_gap_scan_params_t *scan_params;
 	union {
-		/** Scan filter match. */
+		/** Scan filter match parameters. */
 		struct {
 			/** Event structure for @ref BLE_GAP_EVT_ADV_REPORT. This data
 			 * allows the main application to establish connection.
@@ -170,7 +168,7 @@ struct ble_scan_evt {
 		} allow_list_adv_report;
 		/** Advertising report event parameters	when filter is not found. */
 		struct {
-			/** Advertising report */
+			/** Advertising report. */
 			const ble_gap_evt_adv_report_t *report;
 		} not_found;
 		/** Connected event parameters. */
@@ -180,19 +178,20 @@ struct ble_scan_evt {
 			/** Connection handle of the device on which the event occurred. */
 			uint16_t conn_handle;
 		} connected;
-		/** Error event when connecting. Propagates the error code returned by
-		 *  @ref sd_ble_gap_scan_start.
+		/** Connection error event parameters.
+		 *  Propagates the error code returned by @ref sd_ble_gap_scan_start.
 		 */
 		struct {
-			/** Indicates success or failure of an API procedure. In case of
-			 *  failure, a comprehensive error code indicating the cause or reason
-			 *  for failure is provided.
+			/** Error reason.
+			 *  Indicates success or failure of an API procedure. In case of failure,
+			 *  a comprehensive error code indicating the cause or reason for failure
+			 *  is provided.
 			 */
 			int reason;
 		} connecting_err;
-		/** Error event. */
+		/** Error event parameters. */
 		struct {
-			/** Error reason */
+			/** Error reason. */
 			uint32_t reason;
 		} error;
 	} params;
@@ -220,7 +219,7 @@ struct ble_scan_name_filter {
 #endif
 
 #if CONFIG_BLE_SCAN_SHORT_NAME_COUNT > 0
-/** Scan short name filter */
+/** Scan short name filter. */
 struct ble_scan_short_name_filter {
 	struct {
 		/** Short names that the main application will scan for, and that will be
@@ -266,7 +265,7 @@ struct ble_scan_uuid_filter {
 #endif
 
 #if CONFIG_BLE_SCAN_APPEARANCE_COUNT > 0
-/** Scan appearance filter */
+/** Scan appearance filter. */
 struct ble_scan_appearance_filter {
 	/** Apperances that the main application will scan for, and that will be advertised by the
 	 *  peripherals.
@@ -274,7 +273,7 @@ struct ble_scan_appearance_filter {
 	uint16_t appearance[CONFIG_BLE_SCAN_APPEARANCE_COUNT];
 	/** Number of appearances in list. */
 	uint8_t appearance_cnt;
-	/** Flag to inform about enabling or disabling this filter.*/
+	/** Flag to inform about enabling or disabling this filter. */
 	bool appearance_filter_enabled;
 };
 #endif
@@ -283,9 +282,10 @@ struct ble_scan_appearance_filter {
  * @brief Filter data.
  *
  * @details This contains all filter data and the information about enabling and disabling
- * any type of filters. Flag all_filter_mode informs about the filter mode. If this flag is set,
- * then all types of enabled filters must be matched for the library to send a notification to the
- * main application. Otherwise, it is enough to match one of the filters to send a notification.
+ *          any type of filters. Flag all_filter_mode informs about the filter mode. If this flag is
+ *          set, then all types of enabled filters must be matched for the library to send a
+ *          notification to the main application. Otherwise, it is enough to match one of the
+ *          filters to send a notification.
  */
 struct ble_scan_filters {
 #if CONFIG_BLE_SCAN_NAME_COUNT > 0
@@ -314,6 +314,14 @@ struct ble_scan_filters {
 
 #endif /* CONFIG_BLE_SCAN_FILTER */
 
+/**
+ * @defgroup ble_scan_config Scan configuration
+ *
+ * @brief Scan configuration
+ * @{
+ */
+
+/** @brief Default scan parameters for scan configuration. */
 #define BLE_SCAN_SCAN_PARAMS_DEFAULT                                                               \
 {                                                                                                  \
 	.active = 1,                                                                               \
@@ -324,6 +332,7 @@ struct ble_scan_filters {
 	.scan_phys = BLE_GAP_PHY_1MBPS,                                                            \
 }
 
+/** @brief Default connection parameters for scan configuration. */
 #define BLE_SCAN_CONN_PARAMS_DEFAULT                                                               \
 {                                                                                                  \
 	.conn_sup_timeout = BLE_GAP_CP_CONN_SUP_TIMEOUT_MIN,                                       \
@@ -336,15 +345,19 @@ struct ble_scan_filters {
  * @brief Scan instance configuration.
  */
 struct ble_scan_config {
-	/*  BLE GAP scan parameters required to initialize the module. */
+	/** BLE GAP scan parameters required to initialize the module.
+	 *  Can be set to @c BLE_SCAN_SCAN_PARAMS_DEFAULT for default configuration.
+	 */
 	ble_gap_scan_params_t scan_params;
-	/*  If set to true, the module automatically connects after a filter
+	/** If set to true, the module automatically connects after a filter
 	 *  match or successful identification of a device from the allow list.
 	 */
 	bool connect_if_match;
-	/*  Connection parameters. */
+	/** Connection parameters.
+	 *  Can be set to @c BLE_SCAN_CONN_PARAMS_DEFAULT for default configuration.
+	 */
 	ble_gap_conn_params_t conn_params;
-	/*  Variable to keep track of what connection settings will be used
+	/** Variable to keep track of what connection settings will be used
 	 *  if a filer match or an allow list match results in a connection.
 	 */
 	uint8_t conn_cfg_tag;
@@ -352,11 +365,13 @@ struct ble_scan_config {
 	ble_scan_evt_handler_t evt_handler;
 };
 
+/** @} */
+
 /**
- * @brief Scan library instance with options for the different scanning modes.
+ * @brief Scan library instance.
  *
- * @details This structure stores all library settings. It is used to enable or disable scanning
- * modes and to configure filters.
+ * @details This structure stores all library settings.
+ *          Members are not to be set directly by the user.
  */
 struct ble_scan {
 #if defined(CONFIG_BLE_SCAN_FILTER)
@@ -388,14 +403,14 @@ struct ble_scan {
 /**
  * @brief Check if the allow list is used.
  *
- * @param[in] scan_ctx Scan library instance.
+ * @param[in] scan Scan library instance.
  *
  * @return true if allow list is used.
  */
-bool is_allow_list_used(const struct ble_scan *scan_ctx);
+bool is_allow_list_used(const struct ble_scan *scan);
 
 /**
- * @brief Initialize the Scan library.
+ * @brief Initialize the library.
  *
  * @param[out] scan Scan library instance. This structure must be supplied by the application.
  *                  It is initialized by this function and is later used to identify this
@@ -403,7 +418,7 @@ bool is_allow_list_used(const struct ble_scan *scan_ctx);
  * @param[in] config Configuration parameters.
  *
  * @retval NRF_SUCCESS If initialization was successful.
- * @retval NRF_ERROR_NULL When a NULL pointer is passed as input.
+ * @retval NRF_ERROR_NULL If a NULL pointer is passed as input.
  */
 uint32_t ble_scan_init(struct ble_scan *scan, struct ble_scan_config *config);
 
@@ -413,22 +428,22 @@ uint32_t ble_scan_init(struct ble_scan *scan, struct ble_scan_config *config);
  * @details This function starts the scanning according to the configuration set during the
  * initialization.
  *
- * @param[in] scan_ctx Scan library instance.
+ * @param[in] scan Scan library instance.
  *
- * @retval NRF_SUCCESS If scanning started. Otherwise, an error code is returned.
- * @retval NRF_ERROR_NULL If NULL pointer is passed as input.
- *
- * @return This API propagates the error code returned by the
- *         SoftDevice API @ref sd_ble_gap_scan_start.
+ * @retval NRF_SUCCESS If scanning started.
+ * @retval NRF_ERROR_NULL If a NULL pointer is passed as input.
+ * @return In addition, this function may return any error
+ *         returned by the following SoftDevice functions:
+ *         - @ref sd_ble_gap_scan_start()
  */
-uint32_t ble_scan_start(const struct ble_scan *scan_ctx);
+uint32_t ble_scan_start(const struct ble_scan *scan);
 
 /**
  * @brief Stop scanning.
  *
- * @param[in] scan_ctx Scan library instance.
+ * @param[in] scan Scan library instance.
  */
-void ble_scan_stop(const struct ble_scan *scan_ctx);
+void ble_scan_stop(const struct ble_scan *scan);
 
 #if defined(CONFIG_BLE_SCAN_FILTER)
 
@@ -439,46 +454,48 @@ void ble_scan_stop(const struct ble_scan *scan_ctx);
  *          several filters. For example, (BLE_SCAN_NAME_FILTER | BLE_SCAN_UUID_FILTER)
  *          enables UUID and name filters.
  *
- * @param[in] scan_ctx Scan library instance.
+ * @param[in,out] scan Scan library instance.
  * @param[in] mode Filter mode: @c ble_scan_filter_type.
- * @param[in] match_all If this flag is set, all types of enabled filters must be
- *                      matched before generating @ref BLE_SCAN_EVT_FILTER_MATCH to the main
- *                      application. Otherwise, it is enough to match one filter to trigger the
- *                      filter match event.
+ * @param[in] match_all If this flag is set, all types of enabled filters must be matched before
+ *                      generating @ref BLE_SCAN_EVT_FILTER_MATCH to the main application.
+ *                      Otherwise, it is enough to match one filter to trigger the filter match
+ *                      event.
  *
  * @retval NRF_SUCCESS If the filters are enabled successfully.
  * @retval NRF_ERROR_INVALID_PARAM  If the filter mode is invalid.
  * @retval NRF_ERROR_NULL If a NULL pointer is passed as input.
+ * @return In addition, this function may return any error
+ *         returned by the following SoftDevice functions:
+ *         - @ref ble_scan_filters_disable()
  */
-uint32_t ble_scan_filters_enable(struct ble_scan *scan_ctx, uint8_t mode, bool match_all);
+uint32_t ble_scan_filters_enable(struct ble_scan *scan, uint8_t mode, bool match_all);
 
 /**
  * @brief Disable filtering.
  *
  * @details Disable all filters.
- *          Even if the automatic connection establishing is enabled,
- *          the connection will not be established with the first device found after
- *          this function is called.
+ *          Even if the automatic connection establishing is enabled, the connection will not be
+ *          established with the first device found after this function is called.
  *
- * @param[in] scan_ctx Scan library instance.
+ * @param[in,out] scan Scan library instance.
  *
  * @retval NRF_SUCCESS If filters are disabled successfully.
  * @retval NRF_ERROR_NULL If a NULL pointer is passed as input.
  */
-uint32_t ble_scan_filters_disable(struct ble_scan *scan_ctx);
+uint32_t ble_scan_filters_disable(struct ble_scan *scan);
 
 /**
  * @brief Get filter status.
  *
  * @details This function returns the filter setting and whether it is enabled or disabled.
  *
- * @param[in] scan_ctx Scan library instance.
+ * @param[in] scan Scan library instance.
  * @param[out] status Filter status.
  *
  * @retval NRF_SUCCESS If filter status is returned.
  * @retval NRF_ERROR_NULL If a NULL pointer is passed as input.
  */
-uint32_t ble_scan_filter_get(const struct ble_scan *scan_ctx, struct ble_scan_filters *status);
+uint32_t ble_scan_filter_get(const struct ble_scan *scan, struct ble_scan_filters *status);
 
 /**
  * @brief Add scan filter.
@@ -489,7 +506,7 @@ uint32_t ble_scan_filter_get(const struct ble_scan *scan_ctx, struct ble_scan_fi
  *          CONFIG_BLE_SCAN_ADDRESS_COUNT, or @ref CONFIG_BLE_SCAN_APPEARANCE_COUNT, depending on
  *          the filter type, and if the same filter has not already been set.
  *
- * @param[in,out] scan_ctx Scan library instance.
+ * @param[in,out] scan Scan library instance.
  * @param[in] type Filter type.
  * @param[in] data The filter data to add.
  *
@@ -497,12 +514,11 @@ uint32_t ble_scan_filter_get(const struct ble_scan *scan_ctx, struct ble_scan_fi
  * @retval NRF_ERROR_NULL If a NULL pointer is passed as input.
  * @retval NRF_ERROR_DATA_SIZE If the name filter length is too long. Maximum name filter
  *                             length corresponds to @ref NRF_BLE_SCAN_NAME_MAX_LEN.
- * @retval NRF_ERROR_NO_MEMORY If the number of available filters is exceeded.
+ * @retval NRF_ERROR_NO_MEM If the number of available filters is exceeded.
  * @retval NRF_ERROR_INVALID_PARAM If the filter type is incorrect. Available filter types:
  *                                 @ref ble_scan_filter_type_t.
- * @retval BLE_ERROR_GAP_INVALID_BLE_ADDR If the BLE address type is invalid.
  */
-uint32_t ble_scan_filter_add(struct ble_scan *scan_ctx, uint8_t type, const void *data);
+uint32_t ble_scan_filter_add(struct ble_scan *scan, uint8_t type, const void *data);
 
 /**
  * @brief Remove all filters.
@@ -511,11 +527,11 @@ uint32_t ble_scan_filter_add(struct ble_scan *scan_ctx, uint8_t type, const void
  *
  * @note After using this function the filters are still enabled.
  *
- * @param[in,out] scan_ctx Scan library instance.
+ * @param[in,out] scan Scan library instance.
  *
  * @retval NRF_SUCCESS If all filters are removed successfully.
  */
-uint32_t ble_scan_all_filter_remove(struct ble_scan *scan_ctx);
+uint32_t ble_scan_all_filter_remove(struct ble_scan *scan);
 
 #endif /* CONFIG_BLE_SCAN_FILTER */
 
@@ -525,14 +541,13 @@ uint32_t ble_scan_all_filter_remove(struct ble_scan *scan_ctx);
  * @details Use this function to change scanning parameters. During the parameter change
  *          the scan is stopped. To resume scanning, use @ref ble_scan_start.
  *
- * @param[in,out] scan_ctx Scan library instance.
+ * @param[in,out] scan Scan library instance.
  * @param[in] scan_params GAP scanning parameters. Can be initialized as NULL.
  *
  * @retval NRF_SUCCESS If parameters are changed successfully.
  * @retval NRF_ERROR_NULL If a NULL pointer is passed as input.
  */
-uint32_t ble_scan_params_set(struct ble_scan *scan_ctx,
-			     const ble_gap_scan_params_t *scan_params);
+uint32_t ble_scan_params_set(struct ble_scan *scan, const ble_gap_scan_params_t *scan_params);
 
 /**
  * @brief Handler for BLE stack events.
