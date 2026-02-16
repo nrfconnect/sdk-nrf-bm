770d7ce315bb4e3a306bed7410e086d28750e1aa
Emanuele Di Santo <emdi@nordicsemi.no>
2026-02-16 12:30:39 +0100

    storage: bm_storage: support multiple backends
    The library should support having multiple instances operating on
    different backends, for example, the internal NVM on the SoC and
    an external serial memory.
    
    This patch allows choosing the API for an instance during
    initialization. The bm_storage_backend.h header is removed,
    instead an API structure is added.
    
    The non-volatile memory information is copied by the backend
    into the storage structure, rather than being just exported
    by the backend.
    
    Signed-off-by: Emanuele Di Santo <emdi@nordicsemi.no>

--- subsys/storage/bm_storage/native_sim/bm_storage_native_sim.c
+++ subsys/storage/bm_storage/native_sim/bm_storage_native_sim.c
@@ -7,7 +7,13 @@
 #include <string.h>
 #include <stdint.h>
 #include <bm/storage/bm_storage.h>
-#include <bm/storage/bm_storage_backend.h>
+
+static const struct bm_storage_info bm_storage_info = {
+	.erase_unit = 16,
+	.erase_value = 0xFF,
+	.program_unit = 16,
+	.no_explicit_erase = true
+};
 
 static void event_send(const struct bm_storage *storage, struct bm_storage_evt *evt)
 {
@@ -61,7 +67,8 @@ static void write_work_handler(struct k_work *work)
 }
 #endif
 
-int bm_storage_backend_init(struct bm_storage *storage)
+static int bm_storage_native_sim_init(struct bm_storage *storage,
+				      const struct bm_storage_config *config)
 {
 #if defined(CONFIG_BM_STORAGE_BACKEND_NATIVE_SIM_ASYNC)
 	if (!is_queue_init) {
@@ -73,25 +80,27 @@ int bm_storage_backend_init(struct bm_storage *storage)
 		is_queue_init = true;
 	}
 #endif
+
+	storage->nvm_info = &bm_storage_info;
 	return 0;
 }
 
-int bm_storage_backend_uninit(struct bm_storage *storage)
+static int bm_storage_native_sim_uninit(struct bm_storage *storage)
 {
 	/* Nothing to do */
 	return 0;
 }
 
-int bm_storage_backend_read(const struct bm_storage *storage, uint32_t src, void *dest,
-			    uint32_t len)
+static int bm_storage_native_sim_read(const struct bm_storage *storage, uint32_t src, void *dest,
+				      uint32_t len)
 {
 	memcpy(dest, (void *)src, len);
 
 	return 0;
 }
 
-int bm_storage_backend_write(const struct bm_storage *storage, uint32_t dest, const void *src,
-			     uint32_t len, void *ctx)
+static int bm_storage_native_sim_write(const struct bm_storage *storage, uint32_t dest,
+				       const void *src, uint32_t len, void *ctx)
 {
 #if defined(CONFIG_BM_STORAGE_BACKEND_NATIVE_SIM_ASYNC)
 	int err;
@@ -136,8 +145,8 @@ int bm_storage_backend_write(const struct bm_storage *storage, uint32_t dest, co
 #endif
 }
 
-int bm_storage_backend_erase(const struct bm_storage *storage, uint32_t addr, uint32_t len,
-			     void *ctx)
+static int bm_storage_native_sim_erase(const struct bm_storage *storage, uint32_t addr,
+				       uint32_t len, void *ctx)
 {
 	memset((void *)addr, (int)(bm_storage_info.erase_value & 0xFF), len);
 
@@ -155,9 +164,10 @@ int bm_storage_backend_erase(const struct bm_storage *storage, uint32_t addr, ui
 	return 0;
 }
 
-const struct bm_storage_info bm_storage_info = {
-	.erase_unit = 16,
-	.erase_value = 0xFF,
-	.program_unit = 16,
-	.no_explicit_erase = true
+const struct bm_storage_api bm_storage_native_sim_api = {
+	.init = bm_storage_native_sim_init,
+	.uninit = bm_storage_native_sim_uninit,
+	.read = bm_storage_native_sim_read,
+	.write = bm_storage_native_sim_write,
+	.erase = bm_storage_native_sim_erase,
 };
