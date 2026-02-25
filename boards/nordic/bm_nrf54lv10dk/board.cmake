4b2b9ffc0295aeaf4270e6c9c928fc76cd4f608b
Eivind Jølsgard <eivind.jolsgard@nordicsemi.no>
2026-02-25 08:27:16 +0100

    manifest: update sdk-nrf revision
    Update sdk-nrf revision to 928739c2d8ef43e0be1eb0a6ea0ec335144a9929.
    Updates to nRF54lv10a files.
    
    Signed-off-by: Eivind Jølsgard <eivind.jolsgard@nordicsemi.no>

--- boards/nordic/bm_nrf54lv10dk/board.cmake
+++ boards/nordic/bm_nrf54lv10dk/board.cmake
@@ -6,7 +6,7 @@
 
 include(${CMAKE_CURRENT_LIST_DIR}/sysbuild.cmake)
 
-if(CONFIG_SOC_NRF54LV10A_ENGA_CPUAPP)
+if(CONFIG_SOC_NRF54LV10A_CPUAPP)
   board_runner_args(jlink "--device=cortex-m33" "--speed=4000")
 endif()
 
