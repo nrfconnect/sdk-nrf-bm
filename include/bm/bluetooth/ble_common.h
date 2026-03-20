555cb8fe2dff50d3b7345896857f69f72931f8e9
Andreas Moltumyr <andreas.moltumyr@nordicsemi.no>
2026-03-20 15:03:42 +0100

    include: bluetooth: ble_common: add missing doxygen to functions/macros
    Add doxygen to:
        - is_notification_enabled inline function,
        - is_indication_enabled inline function,
        - ble_gap_conn_sec_mode_equal inline function,
        - BYTES_TO_WORDS macro.
    
    Signed-off-by: Andreas Moltumyr <andreas.moltumyr@nordicsemi.no>

--- include/bm/bluetooth/ble_common.h
+++ include/bm/bluetooth/ble_common.h
@@ -23,27 +23,56 @@
 extern "C" {
 #endif
 
-static inline bool is_notification_enabled(const uint8_t *gatts_write_data)
+/**
+ * @brief Calculate how many 32-bit words are needed to hold n_bytes.
+ *
+ * @param[in] n_bytes Number of bytes.
+ *
+ * @return Number of 32-bit words.
+ */
+#define BYTES_TO_WORDS(n_bytes) (((n_bytes) + 3) >> 2)
+
+/**
+ * @brief Read CCCD value from data and check if notifications are enabled.
+ *
+ * @param[in] gatts_value Pointer to CCCD value.
+ *
+ * @return true if notifications are enabled, false if not.
+ */
+static inline bool is_notification_enabled(const uint8_t *gatts_value)
 {
-	const uint16_t cccd_val = sys_get_le16(gatts_write_data);
+	const uint16_t cccd_val = sys_get_le16(gatts_value);
 
 	return (cccd_val & BLE_GATT_HVX_NOTIFICATION);
 }
 
-static inline bool is_indication_enabled(const uint8_t *gatts_write_data)
+/**
+ * @brief Read CCCD value from data and check if indications are enabled.
+ *
+ * @param[in] gatts_value Pointer to CCCD value.
+ *
+ * @return true if indications are enabled, false if not.
+ */
+static inline bool is_indication_enabled(const uint8_t *gatts_value)
 {
-	const uint16_t cccd_val = sys_get_le16(gatts_write_data);
+	const uint16_t cccd_val = sys_get_le16(gatts_value);
 
 	return (cccd_val & BLE_GATT_HVX_INDICATION);
 }
 
+/**
+ * @brief Compare GAP connection security mode.
+ *
+ * @param[in] a First conn_sec_mode value.
+ * @param[in] b Second conn_sec_mode value.
+ *
+ * @return true if the GAP connection security modes are equal, false if not.
+ */
 static inline bool ble_gap_conn_sec_mode_equal(const ble_gap_conn_sec_mode_t *a,
 					       const ble_gap_conn_sec_mode_t *b)
 {
 	return (a->sm == b->sm) && (a->lv == b->lv);
 }
-
-#define BYTES_TO_WORDS(n_bytes) (((n_bytes) + 3) >> 2)
 
 /**
  * @brief Set sec_mode to have no access rights.
