ded4f69685e54b6898d43d236970f57161a902d2
Eivind Jølsgard <eivind.jolsgard@nordicsemi.no>
2026-07-27 13:27:52 +0200

    boards: nordic: add specific jlink device
    Jlink device are now available so we no longer need to use the generic
    cortex-m33 device.
    
    Signed-off-by: Eivind Jølsgard <eivind.jolsgard@nordicsemi.no>

--- boards/nordic/bm_nrf54lm20dk/board.cmake
+++ boards/nordic/bm_nrf54lm20dk/board.cmake
@@ -7,7 +7,7 @@
 include(${CMAKE_CURRENT_LIST_DIR}/sysbuild.cmake)
 
 if(CONFIG_SOC_NRF54LM20A_CPUAPP)
-  board_runner_args(jlink "--device=cortex-m33" "--speed=4000")
+  board_runner_args(jlink "--device=nRF54LM20A_M33" "--speed=4000")
 endif()
 
 include(${ZEPHYR_BASE}/boards/common/nrfutil.board.cmake)
